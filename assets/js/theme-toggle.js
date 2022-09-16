602351c0253cab9ee5a7c826e12ea2c27b6178a0
Christian Oliff <1212885+coliff@users.noreply.github.com>
2022-09-16 15:28:56 +0900

    FIx Theme Toggle JS
    + includes various other minor docs fixes

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,21 +1,48 @@
 // script adapted from Adam Argyle's tutorial: https://web.dev/building-a-theme-switch-component/
 
-const storageKey = "theme-preference",
-  onClick = () => {
-    (theme.value = "light" === theme.value ? "dark" : "light"), setPreference();
-  },
-  getColorPreference = () => (localStorage.getItem(storageKey) ? localStorage.getItem(storageKey) : window.matchMedia("(prefers-color-scheme: light)").matches ? "dark" : "light"),
-  setPreference = () => {
-    localStorage.setItem(storageKey, theme.value), reflectPreference();
-  },
-  reflectPreference = () => {
-    document.firstElementChild.setAttribute("data-theme", theme.value), document.querySelector("#theme-toggle")?.setAttribute("aria-label", theme.value);
-  },
-  theme = { value: localStorage.getItem(storageKey) ? localStorage.getItem(storageKey) : window.matchMedia("(prefers-color-scheme: light)").matches ? "dark" : "light" };
-reflectPreference(),
-  (window.onload = () => {
-    reflectPreference(), document.querySelector("#theme-toggle").addEventListener("click", onClick);
-  }),
-  window.matchMedia("(prefers-color-scheme: light)").addEventListener("change", ({ matches: e }) => {
-    (theme.value = e ? "dark" : "light"), setPreference();
+const storageKey = "theme-preference";
+
+const onClick = () => {
+  theme.value = theme.value === "light" ? "dark" : "light";
+
+  setPreference();
+};
+
+const getColorPreference = () => {
+  if (localStorage.getItem(storageKey)) return localStorage.getItem(storageKey);
+  else
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+};
+
+const setPreference = () => {
+  localStorage.setItem(storageKey, theme.value);
+  reflectPreference();
+};
+
+const reflectPreference = () => {
+  document.firstElementChild.setAttribute("data-theme", theme.value);
+
+  document
+    .querySelector("#theme-toggle")
+    ?.setAttribute("aria-label", theme.value);
+};
+
+const theme = {
+  value: getColorPreference(),
+};
+
+reflectPreference();
+
+window.onload = () => {
+  reflectPreference();
+  document.querySelector("#theme-toggle").addEventListener("click", onClick);
+};
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", ({ matches: isDark }) => {
+    theme.value = isDark ? "dark" : "light";
+    setPreference();
   });
